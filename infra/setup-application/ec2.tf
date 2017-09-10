da0d274761280f468828dda28aa7fdf48f0af63f
kenichishibata <kenichi.shibata3123@gmail.com>
2017-09-10 21:14:48 +0900

    init ec2

---
+++ infra/setup-application/ec2.tf
@@ -0,0 +1,31 @@
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow all ssh inbound traffic"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
+
+resource "aws_security_group" "allow_http" {
+  name        = "allow_http"
+  description = "Allow all http inbound traffic"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
+
+resource "aws_launch_configuration" "as_conf" {
+  name          = "asg_launch_conf"
+  image_id      = "${file("ami-out.file")}"
+  instance_type = "t2.micro"
+}
