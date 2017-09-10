ce208ab58dc4c251dc630e2d84254d009acf06a4
kenichishibata <kenichi.shibata3123@gmail.com>
2017-09-10 22:53:53 +0900

    try asg with elb;

--- infra/setup-application/ec2.tf
+++ infra/setup-application/ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_security_group" "allow_ssh" {
-  name        = "allow_ssh"
+  name        = "allow-ssh"
   description = "Allow all ssh inbound traffic"
 
   ingress {
@@ -12,7 +12,7 @@ resource "aws_security_group" "allow_ssh" {
 }
 
 resource "aws_security_group" "allow_http" {
-  name        = "allow_http"
+  name        = "allow-http"
   description = "Allow all http inbound traffic"
 
   ingress {
@@ -33,7 +33,7 @@ data "aws_ami" "helloawsnode_ami" {
   }
 }
 resource "aws_launch_configuration" "as_conf" {
-  name          = "helloawnode_launch_conf"
+  name          = "helloawnode-launch-conf"
   image_id      = "${data.aws_ami.helloawsnode_ami.image_id}"
   instance_type = "t2.micro"
   security_groups = ["${aws_security_group.allow_ssh.id}","${aws_security_group.allow_http.id}"]
@@ -41,3 +41,46 @@ resource "aws_launch_configuration" "as_conf" {
 }
 
 
+resource "aws_autoscaling_group" "bar" {
+  name                     = "helloawsnode-asg"
+  max_size                  = 6
+  min_size                  = 2
+  health_check_grace_period = 120
+  health_check_type         = "ELB"
+  desired_capacity          = 2
+  force_delete              = true 
+  launch_configuration      = "${aws_launch_configuration.as_conf.name}"
+  load_balancers            = "${aws_elb.helloawsnode_elb.id}"
+  vpc_zone_identifier       = ["file("$path.module")/../../primary-public-out.file", "file("$path.module")/../../secondary-public-out.file"]
+}
+
+# TODO use alb instead for reusability when running multi ports
+resource "aws_elb" "helloawsnode_elb" {
+  name               = "helloawsnode-elb"
+  availability_zones = ["${data.aws_availability_zones.available.names[0]}", "${data.aws_availability_zones.available.names[1]}"]
+
+  listener {
+    instance_port     = 80
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    target              = "HTTP:8000/health"
+    interval            = 30
+  }
+
+  cross_zone_load_balancing   = true
+  idle_timeout                = 400
+  connection_draining         = true
+  connection_draining_timeout = 400
+
+  tags {
+    Name = "helloawsnode-elb"
+  }
+}
