cc339865ffd843ce7d49178b9e5b445fd728d627
kenichi-shibata <kenichi-shibata@outlook.com>
2017-09-11 01:21:11 +0000

    forgot to add egress, by default if egress is not defined, outbound traffic is not permitted   egress {     from_port   = 0     to_port     = 0     protocol    = "-1"     cidr_blocks = ["0.0.0.0/0"]
      }
    https://www.terraform.io/docs/providers/aws/r/default_security_group.html

--- infra/setup-application/ec2.tf
+++ infra/setup-application/ec2.tf
@@ -10,6 +10,14 @@ resource "aws_security_group" "allow_ssh" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
 }
 
 resource "aws_security_group" "allow_http" {
@@ -22,6 +30,14 @@ resource "aws_security_group" "allow_http" {
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+
   }
 
 }
@@ -70,10 +86,10 @@ resource "aws_elb" "helloawsnode_elb" {
 
 
   health_check {
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
-    timeout             = 5
-    target              = "TCP:80/"
+    healthy_threshold   = 4 
+    unhealthy_threshold = 4
+    timeout             = 20
+    target              = "HTTP:80/"
     interval            = 300
   }
 
