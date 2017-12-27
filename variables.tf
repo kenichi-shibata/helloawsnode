687ab761cee4c93225e4319d2d016c35ba33b3de
Kenichi Shibata <kenichi-shibata@outlook.com>
2017-12-27 15:24:59 +0900

    variables: For executing modules

---
+++ variables.tf
@@ -0,0 +1,31 @@
+variable "application_name" {}
+
+variable "instance_type"  {
+  description = "instance type of the first ec2 instance"
+}
+
+variable "aws_region" {
+  description = "aws region"
+  default = "eu-west-2"
+}
+
+variable "pre_tag" {
+  description = "Pre Tag for all the resources can be brand/dept/subdivision environment abbreviation if not prod"
+}
+
+variable "platform_name" {
+  description = "Name of the platform or platform team or maybe even project name if you have high scale project"
+}
+
+variable "environment" {
+  description = "Name of Environment"
+  default = "dev"
+}
+
+variable "service_name" {
+  description = "name of the service or team or cluster of services"
+}
+
+variable "business_region" {
+  description = "business region for the company separate from aws_region"
+}
